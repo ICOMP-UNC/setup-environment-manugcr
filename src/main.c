24cc7c8afe524537df90fb273badde377aa0f861
manugcr <mgilcernich@gmail.com>
2024-03-18 23:47:59 -0300

    Fixing unity tests

--- src/main.c
+++ src/main.c
@@ -5,11 +5,18 @@
 
 int main()
 {
-    printf("Hello, World!\n");
+    printf("--------------------\n");
+    printf("Hello, World! - OSII Lab0 Enviroment setup.\n");
+    printf("--------------------\n\n");
+
+    // Call the function from the include library
     greetings();
 
-    printf("Calling shared lib: %d\n", shared_lib_function("Hello from main.c!"));
-    printf("Calling static lib: %d\n", static_lib_function("Bye from main.c!"));
+    // Call the function from the shared library
+    printf("The lenght of the message is: %d\n", shared_lib_function("Hello from main.c!"));
+
+    // Call the function from the static library
+    printf("The lenght of the message is: %d\n", static_lib_function("Bye from main.c!"));
 
     return 0;
 }
